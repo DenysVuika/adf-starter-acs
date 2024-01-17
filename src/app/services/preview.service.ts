b44502afc233c499b8a85441a44addd578a394f9
Denys Vuika <denys.vuika@gmail.com>
2024-01-17 18:49:29 -0500

    remove unused code

--- src/app/services/preview.service.ts
+++ src/app/services/preview.service.ts
@@ -5,16 +5,7 @@ import { Router } from '@angular/router';
 export class PreviewService {
     private router = inject(Router);
 
-    public content: Blob = null;
-    public name: string = null;
-
     showResource(resourceId: string): void {
         this.router.navigate([{ outlets: { overlay: ['files', resourceId, 'view'] } }]);
-    }
-
-    showBlob(name: string, content: Blob): void {
-        this.name = name;
-        this.content = content;
-        this.router.navigate([{ outlets: { overlay: ['preview', 'blob'] } }]);
     }
 }
