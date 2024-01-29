6508b80645afde38c2115be8bc8ec2aa5c325fb1
Denys Vuika <denys.vuika@gmail.com>
2024-01-29 18:18:19 -0500

    code cleanup

--- src/app/file-view/file-view.component.ts
+++ src/app/file-view/file-view.component.ts
@@ -13,7 +13,6 @@ import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   standalone: true,
