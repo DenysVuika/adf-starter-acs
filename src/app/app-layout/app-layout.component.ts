ab90d904f1809eff015d9b37bb198c13eaf34591
Denys Vuika <denys.vuika@gmail.com>
2024-01-29 17:10:14 -0500

    native material sidenav, code improvements

--- src/app/app-layout/app-layout.component.ts
+++ src/app/app-layout/app-layout.component.ts
@@ -1,7 +1,27 @@
+import { AppConfigModule, LanguageMenuModule } from '@alfresco/adf-core';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatListModule,
+    RouterModule,
+    LanguageMenuModule,
+    TranslateModule,
+    AppConfigModule
+  ],
   templateUrl: './app-layout.component.html',
   styleUrls: ['./app-layout.component.scss']
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MatIconRegistry } from '@angular/material/icon';
 import { AuthModule, CoreModule, TranslateLoaderService, provideTranslations } from '@alfresco/adf-core';
 import { ContentModule } from '@alfresco/adf-content-services';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
@@ -20,30 +21,24 @@ import localeEs from '@angular/common/locales/es';
 registerLocaleData(localeEs);
 
 @NgModule({
-    imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledNonBlocking', relativeLinkResolution: 'legacy' }),
-        // ADF modules
-        AuthModule.forRoot({ useHash: true }),
-        CoreModule.forRoot(),
-        ContentModule.forRoot(),
-        TranslateModule.forRoot({
-            loader: { provide: TranslateLoader, useClass: TranslateLoaderService }
-        })
-    ],
-    declarations: [
-        AppComponent,
-        HomeComponent,
-        LoginComponent,
-        DocumentsComponent,
-        AppLayoutComponent,
-        FileViewComponent
-    ],
-    providers: [
-      provideTranslations('app', 'resources')
-    ],
-    bootstrap: [AppComponent]
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes),
+    AuthModule.forRoot({ useHash: true }),
+    CoreModule.forRoot(),
+    ContentModule.forRoot(),
+    TranslateModule.forRoot({
+      loader: { provide: TranslateLoader, useClass: TranslateLoaderService }
+    }),
+    AppLayoutComponent
+  ],
+  declarations: [AppComponent, HomeComponent, LoginComponent, DocumentsComponent, FileViewComponent],
+  providers: [provideTranslations('app', 'resources')],
+  bootstrap: [AppComponent]
 })
 export class AppModule {
+  constructor(matIconRegistry: MatIconRegistry) {
+    matIconRegistry.setDefaultFontSetClass('material-icons-outlined');
+  }
 }
