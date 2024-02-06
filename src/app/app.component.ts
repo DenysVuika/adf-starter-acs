dba46961c96579e7d54bf7e61688920fa9d01727
Denys Vuika <denys.vuika@gmail.com>
2024-02-06 11:13:09 -0500

    reduce app component files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,22 @@ import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  template: `
+    <router-outlet></router-outlet>
+    <router-outlet name="overlay"></router-outlet>
+  `,
+  styles: [
+    `
+      router-outlet[name='overlay'] + * {
+        width: 100%;
+        height: 100%;
+        z-index: 999999;
+        position: absolute;
+        top: 0;
+        right: 0;
+      }
+    `
+  ]
 })
 export class AppComponent {}
