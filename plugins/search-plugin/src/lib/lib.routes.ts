a6b08963f36d6789f063004a50335ffec967e208
Denys Vuika <denys.vuika@gmail.com>
2024-03-26 11:28:11 -0400

    remove useless provider

--- plugins/search-plugin/src/lib/lib.routes.ts
+++ plugins/search-plugin/src/lib/lib.routes.ts
@@ -1,6 +1,6 @@
 import { Route } from '@angular/router';
 import { SearchPluginComponent } from './search-plugin/search-plugin.component';
-import { ADF_SEARCH_CONFIGURATION, SEARCH_QUERY_SERVICE_TOKEN, SearchQueryBuilderService } from '@alfresco/adf-content-services';
+import { ADF_SEARCH_CONFIGURATION, SearchQueryBuilderService } from '@alfresco/adf-content-services';
 import { DefaultSearchConfiguration } from './search-plugin/search.config';
 
 export const searchPluginRoutes: Route[] = [
@@ -10,8 +10,7 @@ export const searchPluginRoutes: Route[] = [
     providers: [
       // TODO: Search not working without this line
       SearchQueryBuilderService,
-      { provide: ADF_SEARCH_CONFIGURATION, useValue: DefaultSearchConfiguration },
-      { provide: SEARCH_QUERY_SERVICE_TOKEN, useExisting: SearchQueryBuilderService }
+      { provide: ADF_SEARCH_CONFIGURATION, useValue: DefaultSearchConfiguration }
     ]
   }
 ];
